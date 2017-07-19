64b201224497fbaccf50756ef5d3dc502bff9504
khiemnv <khiemnv@hotmail.com>
2017-07-20 00:35:45 +0900

    switch cbid better than callback

--- ConsoleApplication1/csvParser.cs
+++ ConsoleApplication1/csvParser.cs
@@ -210,6 +210,26 @@ namespace ConsoleApplication1
             {myState.s, myState.invalid, myState.z, myState.e, },
             {myState.s, myState.a, myState.z, myState.e, },
         };
+        enum cbid
+        {
+            ss,
+            sa,
+            en,
+            se,
+            aa,
+            es,
+            o1,
+            bs,
+            ba,
+            zz
+        };
+        static cbid[,] cbidTbl = new cbid[5, 4] {
+            {cbid.ss, cbid.sa, cbid.en, cbid.se},
+            {cbid.aa, cbid.o1, cbid.aa, cbid.aa },
+            {cbid.bs, cbid.ba, cbid.en, cbid.o1 },
+            {cbid.es, cbid.o1, cbid.en, cbid.aa },
+            {cbid.ss, cbid.sa, cbid.zz, cbid.se },
+        };
         static myRule[,] clbTbl = new myRule[5, 4] {
             {f_ss, f_sa, f_en, f_se},
             {f_aa, f_01, f_aa, f_aa },
@@ -239,54 +259,101 @@ namespace ConsoleApplication1
                     type = myTkType.t_other;
                     break;
             }
+            executeRule();
+        }
+        void tokenParse4()
+        {
+            type = myTkType.t_other;
+            Debug.Assert(wchr >= 0x10000);
+            wchr -= 0x10000;
+            res.add((char)(0xD800 | (wchr >> 10)));
+            res.add((char)(0xDC00 | (wchr & 0x3FF)));
+
+            //char 1
+            executeRule();
+            //char 2
+            executeRule();
+        }
+        void tokenParse23()
+        {
+            type = myTkType.t_other;
+            Debug.Assert(wchr < 0x10000);
+            res.add((char)(wchr));
+            //char 1
+            executeRule();
+        }
+        void executeRule()
+        {
+            cur = nState;
+            nState = tbl[(int)cur, (int)type];
+            cbid id = cbidTbl[(int)cur, (int)type];
+            switch (id)
+            {
+                case cbid.o1:
+                    //do nonthing
+                    break;
+                case cbid.ss:
+                    res.count++;
+                    res.arr.Add("");
+                    res.curObj.iStart = ++res.curObj.iCur;
+                    break;
+                case cbid.sa:
+                    res.curObj.iStart = ++res.curObj.iCur;
+                    break;
+                case cbid.aa:
+                    res.curObj.iCur++;
+                    break;
+                case cbid.bs:
+                    res.count++;
+                    res.arr.Add(res.gerCurObj());
+                    res.curObj.iCur += 2;
+                    break;
+                case cbid.es:
+                    res.count++;
+                    res.arr.Add(res.gerCurObj());
+                    res.curObj.iCur++;
+                    break;
+                case cbid.ba:
+                    res.curObj.iCur += 2;
+                    res.curObj.dblQt = true;
+                    break;
+                case cbid.en:
+                    res.arr.Add(res.gerCurObj());
+                    //save rec & reset
+                    m_res.Add(res.arr);
+                    m_recCount++;
+                    res.reset();
+                    break;
+                case cbid.se:
+                    res.curObj.iStart = res.curObj.iCur++;
+                    break;
+                case cbid.zz:
+                    res.curObj.iCur++;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+        void executeRule2()
+        {
             cur = nState;
             nState = tbl[(int)cur, (int)type];
             cb = clbTbl[(int)cur, (int)type];
             cb(res, null);
-        }
-        void tokenParse2()
-        {
-            type = myTkType.t_other;
-            if (wchr >= 0x10000)
-            {
-                wchr -= 0x10000;
-                res.add( (char)(0xD800 | (wchr >> 10)));
-                res.add( (char)(0xDC00 | (wchr & 0x3FF)));
-
-                //char 1
-                cur = nState;
-                nState = tbl[(int)cur, (int)type];
-                cb = clbTbl[(int)cur, (int)type];
-                cb(res, null);
-                //char 2
-                cur = nState;
-                nState = tbl[(int)cur, (int)type];
-                cb = clbTbl[(int)cur, (int)type];
-                cb(res, null);
-            }
-            else
-            {
-                res.add((char)(wchr));
-                //char 1
-                cur = nState;
-                nState = tbl[(int)cur, (int)type];
-                cb = clbTbl[(int)cur, (int)type];
-                cb(res, null);
-            }
         }
         myRule cb = null;
         class myCode
         {
             public enum nByte
             {
-                n1,
+                n1 = 1,
                 n2,
                 n3,
                 n4,
                 nx,     //10xxxxx
                 nz,     //invalid
             }
-            byte[] table = new byte[256];
+            public byte[] table = new byte[256];
             public nByte decode(byte b)
             {
                 return (nByte)table[b];
@@ -327,6 +394,10 @@ namespace ConsoleApplication1
         List<myBlock> m_block = new List<myBlock>();
 #endif
         myCode m_code = new myCode();
+        const int block_prefix = 4;
+        const int page_size = 4096;
+        int block_remain = 0;
+        byte[] block = new byte[page_size + block_prefix];
         public void start()
         {
             var name = Path.GetFileName(uri.ToString());
@@ -335,11 +406,9 @@ namespace ConsoleApplication1
             var fs = File.OpenRead(path);
             fs.Seek(0, SeekOrigin.Begin);
 
-            const int block_size = 512;
-            byte[] block = new byte[block_size];
             int nRead;
-            nRead = fs.Read(block, 0, block_size);
-            for (; nRead > 0; nRead = fs.Read(block, 0, block_size))
+            nRead = fs.Read(block, block_prefix, page_size);
+            for (; nRead > 0; nRead = fs.Read(block, block_prefix, page_size))
             {
 #if bg_parse
                 m_block.Add(new myBlock(nRead, block));
@@ -389,64 +458,79 @@ namespace ConsoleApplication1
             //for (; nRead > 0; nRead = fs.Read(block, 0, block_size))
             {
                 processed += nRead;
-
-                for (int i = 0; i < nRead; i++)
+                //start point
+                //  |remain|block read  |
+                //   ^--i
+                int i = block_prefix - block_remain;
+                //calc new remain for next circle
+                //|prefix   |block read |
+                //                     ^--j
+                int iEnd = block_prefix + nRead - 1;
+                int j = iEnd;
+                for (bool loop = true; loop;)
+                {
+                    byte c = m_code.table[block[j]];
+                    switch ((myCode.nByte)c)
+                    {
+                        case myCode.nByte.n1:
+                            //0xxxxxxx
+                            //^--j
+                            j++;
+                            loop = false;
+                            break;
+                        case myCode.nByte.n2:
+                        case myCode.nByte.n3:
+                        case myCode.nByte.n4:
+                            //|1110xxxx 10xxxxxx 10xxxxxx
+                            //|^--j
+                            if ( (j + c) == (iEnd+1))
+                            {
+                                j += c;
+                            }
+                            loop = false;
+                            break;
+                        case myCode.nByte.nx:
+                            //|1110xxxx 10xxxxxx 10xxxxxx
+                            //          ^--j
+                            j--;
+                            break;
+                        default:
+                            throw new NotSupportedException();
+                    }
+                }
+                block_remain = iEnd + 1 - j;
+                //start parse
+                for (; i < j;)
                 {
                     //bin parser
-                    switch (bp_state)
+                    byte nByte = m_code.table[block[i]];
+                    Debug.Assert(nByte <= 4);
+                    Debug.Assert((i + nByte) <= j);
+
+                    switch ((myCode.nByte)nByte)
                     {
-                        case binParserState.s:
-                            {
-                                myCode.nByte nByte = m_code.decode(block[i]);
-                                switch (nByte)
-                                {
-                                    case myCode.nByte.n1:
-                                        bp_state = binParserState.s;
-                                        wchr = block[i];
-
-                                        //token type: q, comma, eol, other
-                                        tokenParse();
-                                        break;
-                                    case myCode.nByte.n2:
-                                        bp_state = binParserState.a2;
-                                        wchr = (block[i] & 0x1F) << 6;
-                                        break;
-                                    case myCode.nByte.n3:
-                                        bp_state = binParserState.b3;
-                                        wchr = (block[i] & 0x0F) << 12;
-                                        break;
-                                    case myCode.nByte.n4:
-                                        bp_state = binParserState.c4;
-                                        wchr = (block[i] & 0x07) << 18;
-                                        break;
-                                    case myCode.nByte.nx:
-                                        break;
-                                    default:
-                                        throw new NotImplementedException();
-                                }
-                            }
+                        case myCode.nByte.n1:
+                            wchr = block[i];
+                            //token type: q, comma, eol, other
+                            tokenParse();
                             break;
-                        case binParserState.a2:
-                            Debug.Assert((block[i] & 0x80) > 0);
-                            wchr |= (block[i] & 0x3F);
-                            bp_state = binParserState.s;
-
-                            //token type: other
-                            tokenParse2();
+                        case myCode.nByte.n2:
+                            wchr = block[i] | ((block[i + 1] & 0x1F) << 6);
+                            tokenParse23();
                             break;
-                        case binParserState.b3:
-                            Debug.Assert((block[i] & 0x80) > 0);
-                            wchr |= (block[i] & 0x3F) << 6;
-                            bp_state = binParserState.a2;
+                        case myCode.nByte.n3:
+                            wchr = block[i] | ((block[i + 1] & 0x1F) << 6) | ((block[i + 2] & 0x0F) << 12);
+                            tokenParse23();
                             break;
-                        case binParserState.c4:
-                            Debug.Assert((block[i] & 0x80) > 0);
-                            wchr |= (block[i] & 0x3F) << 12;
-                            bp_state = binParserState.b3;
+                        case myCode.nByte.n4:
+                            wchr = block[i] | ((block[i + 1] & 0x1F) << 6) | ((block[i + 2] & 0x0F) << 12) | ((block[i + 3] & 0x07) << 18);
+                            tokenParse4();
                             break;
+                        default:
+                            throw new NotImplementedException();
                     }
                     //token parser
-
+#if use_callback
                     //crt record
                     if (cb == f_en)
                     {
@@ -454,6 +538,18 @@ namespace ConsoleApplication1
                         m_recCount++;
                         res.reset();
                     }
+#endif
+                    //next
+                    i += nByte;
+                }
+
+                //save remain
+                if (block_remain > 0)
+                {
+                    //         |<block size>      |
+                    //|prefix  |parsed byte|remain|
+                    //  |remain|            ^--i
+                    Buffer.BlockCopy(block, i, block, block_prefix - block_remain, block_remain);
                 }
             }
             //fs.Close();
